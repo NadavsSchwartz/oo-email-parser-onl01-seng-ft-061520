13782b98025c03ce3b74d49aed4aa74986096df6
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-28 20:26:21 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,7 +12,11 @@ class EmailAddressParser
   
   def parse
       parsed_email = email.delete(',')
+<<<<<<< HEAD
       parsed_email.split.uniq
+=======
+      parsed_email.split(" ")
+>>>>>>> 326f99b4afc8a49e2f76e0f3c63a0eea4ada261d
   end
   
 end
